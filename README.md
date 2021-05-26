c07e07f3293996768450496fe1e0bd78517f4ca1
Alexander Günsche <git@lxg.de>
2021-05-26 23:02:53 +0200

    create CommonJS exports as well

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # l10n-cldr – translated locale data
 
-This is basically a frontend to the CLDR data. It can either be used as a library, providing locale data as ES module. Or as a command line tool generating JSON files.
+This is basically providing CLDR data as CommonJS and ES modules.
--- bin/generate.mjs
+++ bin/generate.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as fs from 'fs'
+import rimraf from 'rimraf'
 import fg from "fast-glob"
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -8,14 +9,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const basePath = fs.realpathSync(`${__dirname}/..`)
 const nodePath = `${basePath}/node_modules`
-const cldrPath = `${basePath}/dist/cldr`
+const cjsCldrPath = `${basePath}/dist/cjs/cldr`
+const esmCldrPath = `${basePath}/dist/esm/cldr`
 const validLocale = /^[a-z]{2}(-[A-Z]{2})?$/
-
-if (!fs.existsSync(cldrPath)) {
-    fs.mkdirSync(cldrPath, { recursive: true })
-}
-
-fg.sync("*", { cwd: cldrPath }).forEach(file => fs.unlinkSync(`${cldrPath}/${file}`))
 
 const cldrFirstDay = JSON.parse(fs.readFileSync(`${nodePath}/cldr-core/supplemental/weekData.json`))
     .supplemental.weekData.firstDay
@@ -54,6 +50,11 @@ fg.sync("**/territories.json", { cwd: cldrNamesPath }).forEach(file => {
     }
 })
 
+;[cjsCldrPath, esmCldrPath].forEach(path => {
+    fs.existsSync(path) || fs.mkdirSync(path, { recursive: true })
+    fs.readdirSync(path).length && rimraf.sync(`${path}/*`)
+})
+
 Object.keys(cldrDateFiles).forEach(locale => {
     const country = locale.substr(3, 2)
 
@@ -67,13 +68,23 @@ Object.keys(cldrDateFiles).forEach(locale => {
         }
 
         fs.writeFileSync(
-            `${cldrPath}/${locale}.mjs`,
+            `${esmCldrPath}/${locale}.mjs`,
             `export const countries = ${JSON.stringify(data.countries)}\n` +
             `export const months = ${JSON.stringify(Object.values(data.months))}\n` +
             `export const days = ${JSON.stringify(Object.values(data.days))}\n` +
             `export const daysShort = ${JSON.stringify(Object.values(data.daysShort))}\n` +
             `export const monthsShort = ${JSON.stringify(Object.values(data.monthsShort))}\n` +
             `export const firstday = ${firstDays[cldrFirstDay[country] || "mon"]}\n`
+        )
+
+        fs.writeFileSync(
+            `${cjsCldrPath}/${locale}.cjs`,
+            `exports.countries = ${JSON.stringify(data.countries)}\n` +
+            `exports.months = ${JSON.stringify(Object.values(data.months))}\n` +
+            `exports.days = ${JSON.stringify(Object.values(data.days))}\n` +
+            `exports.daysShort = ${JSON.stringify(Object.values(data.daysShort))}\n` +
+            `exports.monthsShort = ${JSON.stringify(Object.values(data.monthsShort))}\n` +
+            `exports.firstday = ${firstDays[cldrFirstDay[country] || "mon"]}\n`
         )
     }
 })
