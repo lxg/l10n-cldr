76ec9435a9da607aa8653edac3d4bf64a8a4b8a9
amroman87 <aromangodinez@gmail.com>
2021-08-05 17:00:27 +0200

    Update cldr.js

--- src/esm/cldr.js
+++ src/esm/cldr.js
@@ -1,22 +1,25 @@
 import * as fs from 'fs'
 import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { fileURLToPath, pathToFileURL } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const basePath = fs.realpathSync(`${__dirname}/../..`)
 const esmDataPath = `${basePath}/dist/esm/data`
+ 
+
 
 export async function getLocaleData(locale) {
     let file
-
-    try {
-        file = await import(`${esmDataPath}/${locale}.js`)
+ 
+    try {        
+        file = await import(pathToFileURL(`${esmDataPath}/${locale}.js`).href)
     } catch {
          try {
-             file = await import(`${esmDataPath}/${locale.substr(0, 2)}.js`)
+             file = await import(pathToFileURL(`${esmDataPath}/${locale.substr(0, 2)}.js`).href)
         } catch (e) {
+            console.log(e);
             throw new Error(`No CLDR data found for ${locale}.`)
         }
     }
-
+    
     return file
 }
