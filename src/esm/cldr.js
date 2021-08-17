03b41b24910f8c5c42d76666956b4ab461a973e2
Alexander Günsche <git@lxg.de>
2021-08-17 12:45:35 +0200

    install linter and fix whitespace

--- src/esm/cldr.js
+++ src/esm/cldr.js
@@ -4,13 +4,11 @@ import { fileURLToPath, pathToFileURL } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const basePath = fs.realpathSync(`${__dirname}/../..`)
 const esmDataPath = `${basePath}/dist/esm/data`
- 
-
 
 export async function getLocaleData(locale) {
     let file
- 
-    try {        
+
+    try {
         file = await import(pathToFileURL(`${esmDataPath}/${locale}.js`).href)
     } catch {
          try {
@@ -20,6 +18,6 @@ export async function getLocaleData(locale) {
             throw new Error(`No CLDR data found for ${locale}.`)
         }
     }
-    
+
     return file
 }
